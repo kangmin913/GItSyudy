0c00c0a075df9d49681f00cf200cb760e9ff1c36
kangmin913 <kangmin913@naver.com>
2022-12-20 12:00:56 +0900

    message

---
+++ Git_Project.cpp
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main()
+{
+	srand(time(NULL));
+	
+	int num1,num2,help;
+	int ans,ans2,check = 0;
+	char word;
+	while(check!=11)
+	{
+		num1 = rand()%20;
+		num2 = rand()%20;
+		help = rand()%4;
+		
+		if(help ==0)
+		{
+			word = '+';
+			ans2 = num1 + num2;
+		}
+		
+		else if(help ==1)
+		{
+			word = '-';
+			ans2 = num1 - num2;
+		}
+		else if(help ==2)
+		{
+			word = '*';
+			ans2 = num1 * num2;
+		}
+		else if(help ==3)
+		{
+			word = '/';
+			ans2 = num1 / num2;
+		}
+		
+		
+		printf("[%d/10] %d %c %d = ",check,num1,word,num2);
+		scanf("%d",&ans);
+		if(ans == ans2)
+		{
+			printf("Correct!\n");
+			++check;
+		}
+		else
+		{
+			printf("Wrong! Answer is %d\n",ans2);
+		}
+		
+
+	}
+
+	
+}
